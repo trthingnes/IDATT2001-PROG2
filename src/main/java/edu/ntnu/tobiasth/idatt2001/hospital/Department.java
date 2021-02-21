3a0083e59f721bdd7dbfe222ff38c3a48a21c617
Tobias Rødahl Thingnes <tobias@thingn.es>
2021-02-21 21:59:22 +0100

    Add Maven Javadoc plugin

--- src/main/java/edu/ntnu/tobiasth/idatt2001/hospital/Department.java
+++ src/main/java/edu/ntnu/tobiasth/idatt2001/hospital/Department.java
@@ -2,6 +2,10 @@ package edu.ntnu.tobiasth.idatt2001.hospital;
 
 import java.util.*;
 
+/**
+ * Model of a hospital department.
+ * Belongs to a {@link Hospital} object and has objects of type {@link Patient} and {@link Employee}.
+ */
 public class Department {
     private String departmentName;
     private final HashMap<String, Patient> patients = new HashMap<>();
@@ -11,30 +15,57 @@ public class Department {
         this.departmentName = departmentName;
     }
 
+    /**
+     * @return Department name
+     */
     public String getDepartmentName() {
         return departmentName;
     }
 
+    /**
+     * @param departmentName New department name
+     */
     public void setDepartmentName(String departmentName) {
         this.departmentName = departmentName;
     }
 
+    /**
+     * Gets a list of all the {@link Patient} objects for this department.
+     * @return List of patients
+     */
     public List<Patient> getPatients() {
         return new ArrayList<>(patients.values());
     }
 
+    /**
+     * Adds a new patient to this department.
+     * @param patient Patient to add
+     */
     public void addPatient(Patient patient) {
         patients.put(patient.getPersonalIdNumber(), patient);
     }
 
+    /**
+     * Gets a list of all the {@link Employee} objects for this department.
+     * @return List of employees
+     */
     public List<Employee> getEmployees() {
         return new ArrayList<>(employees.values());
     }
 
+    /**
+     * Adds a new employee to this department.
+     * @param employee Employee to add
+     */
     public void addEmployee(Employee employee) {
         employees.putIfAbsent(employee.getPersonalIdNumber(), employee);
     }
 
+    /**
+     * Removes a {@link Patient} or {@link Employee} object from the respective map.
+     * @param person {@link Person} object to remove.
+     * @throws RemoveException If the given person cannot be found.
+     */
     public void removePerson(Person person) throws RemoveException {
         Optional<Patient> patient = patients.values().stream().filter(person::equals).findFirst();
         Optional<Employee> employee = employees.values().stream().filter(person::equals).findFirst();
@@ -47,6 +78,11 @@ public class Department {
         employee.ifPresent(value -> employees.remove(value.getPersonalIdNumber()));
     }
 
+    /**
+     * Checks if two {@link Department} objects are equal.
+     * @param o Another object
+     * @return True if objects are equal, false if not.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
